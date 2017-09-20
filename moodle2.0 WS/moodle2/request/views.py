cdd085ac6a76bbb554a69946568eb765e42c39a8
servidoryei <servidoryei@hotmail.com>
2017-09-20 13:41:12 -0500

    found how to get values from querys

--- moodle2.0 WS/moodle2/request/views.py
+++ moodle2.0 WS/moodle2/request/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from moodle2 import dbParams as connection
+import moodle2.ORM as ORM
+from moodle2.dbParams import *
 
 
 # Create your views here.
 def authentication(request, usr, pwd):
-	connection.cur.ex
+	if 	cur.execute("SELECT username, pwd FROM user WHERE `username`="+usr+"AND `password="+pwd) == 1:
+		return True
+	else:
+		return False
+
+def groups(request):
+	cur._query("SELECT * FROM group WHERE id")
--- moodle2.0 WS/moodle2/user/views.py
+++ moodle2.0 WS/moodle2/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import moodle2.ORM as ORM
+from moodle2.dbParams import *
 # Create your views here.
 # Views Handles web service JSON responses
 
@@ -8,7 +9,8 @@ def sdad(request):
 	arg1="hola"
 	json1  = JSONEncoder.encode(arg1)
 	return json1.__str__
+
 def test(request):
-	course1 = ORM.course(1,"Math","Kaeko")
-	return JsonResponse(course1.__json__())
-	
+	response = ORM.course(1,"Math","Kaeko")
+	return JsonResponse(response.__json__())
+
